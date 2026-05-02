2631f1dd9c9402cb78544e8b8587ace6fa431ebb
YangYuS8 <YangYuS8@163.com>
2026-05-03 04:18:58 +0800

    fix: build Astro site before deploying Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://yangyus8.top',
   integrations: [tailwind()],
 });
